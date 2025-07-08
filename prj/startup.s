f0f6b804ec541b2126f8da8d6f9bc397e8c316a0
caesar-avgvstvs <208864582+caesar-avgvstvs@users.noreply.github.com>
2025-07-08 10:31:44 +0530

    Commit Jul08

--- prj/startup.s
+++ prj/startup.s
@@ -24,8 +24,44 @@ _vector_base:
 	.word 0
 	.word SWHandler
 	.word 0
+
 	/* External Interrupts (Defined in the CH32V203 manual) */
  /* Not gonna handle any of the external interrupts as of now */
+	.option rvc
+	.section .text.vector_handler, "ax",@progbits
+	.weak NMIHandler
+	.weak hardFaultHandler
+	.weak ecallMModeHandler
+	.weak ecallUModeHandler
+	.weak breakPointHandler
+	.weak sysTickHandler
+	.weak SWHandler
 
- .option rvc
- .section .vector_handler, "ax",@progbits
+NMIHandler:
+hardFaultHandler:
+ecallMModeHandler:
+ecallUModeHandler:
+breakPointHandler:
+sysTickHandler:
+SWHandler:
+1:
+	j 1b
+
+
+	.section .text.handler_reset,"ax", @progbits
+	.weak handle_reset
+	.align 1
+handle_reset:
+	.option push
+	.option norelax
+	la gp, __global_pointer$
+	.option pop
+1:
+	la sp, _eusrstack
+2:
+	la a0, _data_lma
+	la a1, _data_vma
+	la a2, _edata
+	bgeu a1, a2, 2f
+1:
+	lw t0, (a0)
